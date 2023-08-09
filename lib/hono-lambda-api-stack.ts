ada756fa72e21a67a9203615dff7b7a46fe83ca7
son.tran <son.tran@team.enouvo.com>
2023-08-09 14:53:13 +0700

    feat(main): add cloudfront

--- lib/hono-lambda-api-stack.ts
+++ lib/hono-lambda-api-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class HonoLambdaApiStack extends cdk.Stack {
@@ -19,8 +21,19 @@ export class HonoLambdaApiStack extends cdk.Stack {
     fn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     });
-    new apigw.LambdaRestApi(this, 'myapi', {
+
+    const api = new apigw.LambdaRestApi(this, 'myapi', {
       handler: fn,
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'distribution', {
+      defaultBehavior: {
+        origin: new origins.HttpOrigin(api.url),
+      },
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
+      value: distribution.distributionDomainName,
     });
   }
 }
